1aa7c7f04dad9467a51f24b386a4156703f104c5
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-01-08 01:57:15 -0500

    Add Java Stdin and Stdout II

--- run/java/src/Main.java
+++ run/java/src/Main.java
@@ -20,28 +20,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int N = sc.nextInt();
-        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine(); // Consume newline left-over
+        String s = sc.nextLine();
 
-        /*
-        Given an integer, n , perform the following conditional actions:
-
-        If n is odd, print Weird
-        If n is even and in the inclusive range of 2 to 5 , print Not Weird
-        If n is even and in the inclusive range of 6 to 20, print Weird
-        If n is even and greater than 20, print Not Weird
-         */
-
-        if (N % 2 == 1) {
-            System.out.println("Weird");
-        } else if (N >= 2 && N <= 5) {
-            System.out.println("Not Weird");
-        } else if (N >= 6 && N <= 20) {
-            System.out.println("Weird");
-        } else {
-            System.out.println("Not Weird");
-        }
-
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 
         sc.close();
     }
