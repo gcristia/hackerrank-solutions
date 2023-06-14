f0ef0364e02f0c18a52005028a507bc0c949bf55
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-06-14 12:32:24 -0400

    Add SQL/Challenges

--- practice/sql/Challenges/file.sql
+++ practice/sql/Challenges/file.sql
@@ -1,24 +1,11 @@
-select H.hacker_id, H.name, count(C.challenge_id) as total_count
-from Hackers H
-         join Challenges C
-              on H.hacker_id = C.hacker_id
-group by H.hacker_id, H.name
-having total_count =
-       (select count(temp1.challenge_id) as max_count
-        from challenges temp1
-        group by temp1.hacker_id
-        order by max_count desc
-    limit 1
-    )
-    or total_count in
-     (
-select distinct other_counts
-from (
-    select H2.hacker_id, H2.name, count (C2.challenge_id) as other_counts
-    from Hackers H2 join Challenges C2
-    on H2.hacker_id = C2.hacker_id
-    group by H2.hacker_id, H2.name
-    ) temp2
-group by other_counts
-having count (other_counts) =1)
-order by total_count desc, H.hacker_id
+WITH PARTITIONED AS (SELECT c.hacker_id,
+                            COUNT(c.challenge_id) AS count_chal,
+                            ROW_NUMBER()             OVER (PARTITION BY COUNT(c.challenge_id) ORDER BY c.hacker_id) AS rown
+                     FROM Challenges c
+                     GROUP BY c.hacker_id)
+SELECT h.hacker_id, h.name, p.count_chal
+FROM Hackers h
+         INNER JOIN PARTITIONED p ON h.hacker_id = p.hacker_id
+WHERE (p.count_chal = (SELECT MAX(count_chal) FROM PARTITIONED) OR
+       p.count_chal NOT IN (SELECT count_chal FROM PARTITIONED where rown = 2))
+ORDER BY p.count_chal desc, h.hacker_id;
